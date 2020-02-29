b6017049a8d5fc2f3b8cdbd6a1b2b6126af4aa9e
Hoang Duy Tran <hoangduytran1960@googlemail.com>
2020-02-29 19:02:30 +0000

    Added uppo.sh, replacing make update_po

---
+++ bin/uppo.sh
@@ -0,0 +1,9 @@
+cd $BLENDER_MAN_EN
+cd locale/vi
+svn cleanup
+svn update
+cd $BLENDER_MAN_EN
+rm -fr build/locale
+make gettext
+sphinx-intl --config=manual/conf.py update --pot-dir=build/locale --language="vi"
+python3 tools_rst/rst_check_locale.py
