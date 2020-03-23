d4a0bce4d3bf12bc744e4c56602db4fb4dfe7467
Hoang Duy Tran <hoangduytran1960@googlemail.com>
2020-03-23 03:38:04 +0000

    Latest changes, including the adding of letter case transforms in findtext.py

--- bin/findtext.py
+++ bin/findtext.py
@@ -12,13 +12,31 @@ import datetime
 from time import gmtime, strftime, time
 from pytz import timezone
 from argparse import ArgumentParser
-from pprint import pprint as pp
+from pprint import pprint
 from sphinx_intl import catalog as c
 from babel.messages import pofile
-import enum
+from enum import Enum
 import chardet
 
+DEBUG = True
+def pp(object, stream=None, indent=1, width=80, depth=None, *args, compact=False):
+    if DEBUG:
+        pprint(object, stream=stream, indent=indent, width=width, depth=depth, *args, compact=compact)
+        print('-' * 30)
+
+def _(*args, **kwargs):
+    if DEBUG:
+        print(args, kwargs)
+        print('-' * 30)
+
 INVERT_SEP='•••'
+
+class LetterCase(Enum):
+    NO_CHANGE="No Change",
+    LOWER_CASE="Lower",
+    UPPER_CASE="Upper",
+    CAPITAL_CASE="Capital",
+    TITLE_CASE="Title"
 
 class ListPathEvent:
     def __init__(self):
@@ -74,13 +92,13 @@ class findFileByExtension(ListPathEvent):
         return os.path.getmtime(path)
 
     def getListSorted(self, key_function=None, is_reversed=False):
-        #print("getListSorted:", self.result)
+        #_("getListSorted:", self.result)
         #pp(self.result)
-        #print("before sorted getListSorted")
+        #_("before sorted getListSorted")
 
         sorted_list = sorted(self.result, key=key_function, reverse=is_reversed)
         #pp(sorted_list)
-        #print("after sorted getListSorted")
+        #_("after sorted getListSorted")
         return sorted_list
 
     def getListSortedModified(self, modified_within_last_n_seconds=0):
@@ -107,8 +125,8 @@ class BasicIO:
             with open(file_name, "w+", encoding="utf-8") as f:
                 f.write(data)
         except Exception as e:
-            print("ERROR write data:", data)
-            print("ERROR write file:", file_name)
+            _("ERROR write data:", data)
+            _("ERROR write file:", file_name)
             raise e
 
     def readTextFile(self, file_name):
@@ -118,7 +136,7 @@ class BasicIO:
                 data = f.read()
             return data
         except Exception as e:
-            print("ERROR reading file:", file_name, e)
+            _("ERROR reading file:", file_name, e)
             raise e
 
     # -----------------------------------------------------------------------------
@@ -132,7 +150,7 @@ class BasicIO:
 
             valid_function = ((not callback is None) and (isinstance(callback, ListPathEvent)))
             if (valid_function):
-                # print("calling back with:", dirpath, dirnames, filenames)
+                # _("calling back with:", dirpath, dirnames, filenames)
                 callback.setVars(dirpath, dirnames, filenames)
                 callback.run()
 
@@ -142,8 +160,27 @@ class FindFilesHasPattern:
         self.basic_io = BasicIO()
         self.found_lines_dic = {}
 
+    def getCase(self, lower_case, upper_case, capital_case, title_case):
+        actual_case = LetterCase.NO_CHANGE
+        if lower_case is not None:
+            actual_case = LetterCase.LOWER_CASE
+        elif upper_case is not None:
+            actual_case = LetterCase.UPPER_CASE
+        elif capital_case is not None:
+            actual_case = LetterCase.CAPITAL_CASE
+        elif title_case is not None:
+            actual_case = LetterCase.TITLE_CASE
+        else:
+            actual_case = LetterCase.NO_CHANGE
+        return actual_case
+
     def setVars(self,
             find_pattern,
+            replace_pattern,
+            lower_case,
+            upper_case,
+            capital_case,
+            title_case,
             find_file,
             find_po,
             find_rst,
@@ -154,7 +191,9 @@ class FindFilesHasPattern:
             after_lines,
             only_match,
             show_line_number,
-            invert_match
+            invert_match,
+            testing_only,
+            debugging
             ):
         self.find_file = (find_file if (find_file and os.path.isfile(find_file)) else None)
         self.find_po = (True if find_po else False)
@@ -162,21 +201,35 @@ class FindFilesHasPattern:
         self.find_py = (True if find_py else False)
         self.find_src = (True if find_src else False)
         self.case_sensitive = (True if case_sensitive else False)
+        DEBUG = (True if debugging else False)
 
-        flag = (re.I if not self.case_sensitive else 0)
+        self.letter_case = self.getCase(lower_case, upper_case, capital_case, title_case)
+
+        self.pattern_flag = (re.I if not self.case_sensitive else 0)
         #self.input_pattern = find_pattern
-        self.find_pattern = re.compile(r'{}'.format(find_pattern), flags=flag)
+        if (find_pattern is not None):
+            self.find_pattern = re.compile(r'{}'.format(find_pattern), flags=self.pattern_flag)
+            self.txt_find_pattern = find_pattern
+        else:
+            self.find_pattern = None
+
+        if (replace_pattern is not None):
+            self.replace_pattern = replace_pattern
+        else:
+            self.replace_pattern = None
+
         self.from_line = (int(before_lines) if before_lines else -1)
         self.to_line = (int(after_lines) if after_lines else -1)
         self.only_match = (True if only_match else False)
         self.show_line_number = (True if show_line_number else False)
         self.invert_match = (True if invert_match else False)
+        self.testing_only = (True if testing_only else False)
 
     def patternMatchAll(self, pat, text):
         try:
             # itor = pat.finditer(text)
-            # print("itor", type(itor))
-            # print("dir", dir(itor))
+            # _("itor", type(itor))
+            # _("dir", dir(itor))
 
             for m in pat.finditer(text):
                 original = ()
@@ -248,20 +301,20 @@ class FindFilesHasPattern:
         po_dir = os.environ['BLENDER_MAN_VI']
         if po_dir:
             po_dir = os.path.join(po_dir, "LC_MESSAGES")
-            print("po_dir:", po_dir)
+            _("po_dir:", po_dir)
 
         rst_dir = os.environ['BLENDER_MAN_EN']
         if rst_dir:
             rst_dir = os.path.join(rst_dir, "manual")
-            print("rst_dir:", rst_dir)
+            _("rst_dir:", rst_dir)
 
         py_dir = os.environ['LOCAL_PYTHON_3']
         if py_dir:
-            print("py_dir:", py_dir)
+            _("py_dir:", py_dir)
 
         src_dir = os.environ['BLENDER_SRC']
         if src_dir:
-            print("src_dir:", src_dir)
+            _("src_dir:", src_dir)
 
         po_file_list=None
         rst_file_list=None
@@ -273,20 +326,20 @@ class FindFilesHasPattern:
         else:
             if (self.find_po):
                 po_file_list = self.getFileList(po_dir, ".po")
-                #print("po_file_list")
-                ##pp(po_file_list)
+                _("po_file_list")
+                pp(po_file_list)
                 search_file_list.extend(po_file_list)
 
             if (self.find_rst):
                 rst_file_list = self.getFileList(rst_dir, ".rst")
-                #print("rst_file_list")
-                ##pp(rst_file_list)
+                _("rst_file_list")
+                pp(rst_file_list)
                 search_file_list.extend(rst_file_list)
 
             if (self.find_py):
                 py_file_list = self.getFileList(py_dir, ".py")
-                #print("py_file_list")
-                ##pp(py_file_list)
+                _("py_file_list")
+                pp(py_file_list)
                 search_file_list.extend(py_file_list)
 
             if (self.find_src):
@@ -295,7 +348,7 @@ class FindFilesHasPattern:
                 cpp_list = self.getFileList(src_dir, ".cpp")
                 c_list = self.getFileList(src_dir, ".c")
                 h_list = self.getFileList(src_dir, ".h")
-                #print("py_file_list")
+                #_("py_file_list")
                 ##pp(py_file_list)
                 #pp(py_list)
                 #pp(cc_list)
@@ -312,17 +365,19 @@ class FindFilesHasPattern:
 
         has_file = (len(search_file_list) > 0)
         if not has_file:
-            print("No files to search! Terminate.")
+            _("No files to search! Terminate.")
             return
 
-        # print("SEARCHING:")
+        is_po_only = (self.find_po and not (self.find_py or self.find_rst or self.find_src))
+        is_replace = ((self.replace_pattern is not None) and is_po_only) or (self.letter_case != LetterCase.NO_CHANGE)
+
+        # _("SEARCHING:")
         for f in search_file_list:
-            #print(f)
-            try:
-                # print("searching in:", f, "for", self.find_pattern)
+            _("processing:", f, "for", self.find_pattern)
+            if is_replace:
+                self.replaceInPOFile(f)
+            else:
                 self.findPatternInFile(f)
-            except Exception as e:
-                print("Exception:", e, " file:", f)
 
     def listingRange(self, data_list, found_index):
         has_from_line = (self.from_line >= 0)
@@ -332,19 +387,21 @@ class FindFilesHasPattern:
         if has_from_line:
             min_range = [0, found_index - self.from_line]
             from_line = max(min_range)
-            #print("from_line", from_line)
+            _("from_line", from_line)
         else:
             from_line = found_index
+            _("from_line", from_line)
 
         if has_to_line:
             max_range = [found_index + self.to_line + 1, max_lines]
             to_line = min(max_range)
-            #print("to_line", to_line)
+            _("to_line", to_line)
         else:
             max_range = [found_index + 1, max_lines]
             to_line = min(max_range)
+            _("to_line", to_line)
 
-        #print("found_index", found_index, "from_line", from_line, "to_line", to_line)
+        _("found_index", found_index, "from_line", from_line, "to_line", to_line)
         for index in range(from_line, to_line):
             text_line = data_list[index]
             if self.only_match:
@@ -359,7 +416,7 @@ class FindFilesHasPattern:
 
             entry = {index: match_text}
             self.found_lines_dic.update(entry)
-            #print(index, text_line)
+            _(entry)
 
 
     def reportFind(self, data):
@@ -402,8 +459,66 @@ class FindFilesHasPattern:
         if is_found:
             #pp(found_list)
             self.reportFind(data)
-            print("Found in:", file_path)
-            print("-" * 80)
+            _("Found in:", file_path)
+            _("-" * 80)
+
+    def switchExistingTextsCase(self, txt):
+        #print("switchExistingTextsCase", txt, type(txt))
+
+        changed_count = 0
+        new_txt = str(txt)
+        for orig, _ in self.patternMatchAll(self.find_pattern, txt):
+            s, e, found_txt = orig
+            orig_txt = str(found_txt)
+            if self.letter_case == LetterCase.LOWER_CASE:
+                found_txt = found_txt.lower()
+            elif self.letter_case == LetterCase.UPPER_CASE:
+                found_txt = found_txt.upper()
+            elif self.letter_case == LetterCase.CAPITAL_CASE:
+                first_letter = found_txt[0]
+                first_letter = first_letter.upper()
+                found_txt = first_letter + found_txt[1:]
+            elif self.letter_case == LetterCase.TITLE_CASE:
+                found_txt = found_txt.title()
+
+            new_txt = new_txt[:s] + found_txt + new_txt[e:]
+            is_changed = (found_txt != orig_txt)
+            changed_count += (1 if is_changed else 0)
+            #_("switchExistingTextsCase from:", txt, "=>", new_txt, changed_count)
+        return new_txt, changed_count
+
+
+    def replaceInPOFile(self, file_path):
+        changed = False
+        po_cat = c.load_po(file_path)
+        for index, m in enumerate(po_cat):
+            is_ignore = (index == 0)
+            if is_ignore:
+                continue
+
+            old_translation = m.string
+            has_translation = (old_translation and (len(old_translation) > 0 ))
+            if not has_translation:
+                continue
+
+            is_changing_case = (self.letter_case != LetterCase.NO_CHANGE)
+            if is_changing_case:
+                new_translation, n_changed = self.switchExistingTextsCase(old_translation)
+            else:
+                new_translation, n_changed = re.subn(self.txt_find_pattern, self.replace_pattern, old_translation, flags=self.pattern_flag)
+            has_changed = (n_changed > 0)
+            if has_changed:
+                m.string = new_translation
+                changed = True
+                _("Replaced:")
+                _(f'old msgstr \"{old_translation}\"')
+                _(f'new msgstr \"{new_translation}\"')
+
+        is_writing_changes = (changed and not self.testing_only)
+        if is_writing_changes:
+            c.dump_po(file_path, po_cat)
+            _("Wrote changes to:", file_path)
+            _("-" * 80)
 
     def getFileList(self, directory, extension):
         mod_file_list = []
@@ -417,13 +532,20 @@ class FindFilesHasPattern:
         return mod_file_list
 
     def run(self):
-        # print("Hoang Duy Tran")
+        # _("Hoang Duy Tran")
         self.find()
 
 
 parser = ArgumentParser()
 #parser.add_argument("-c", "--clean", dest="clean_action", help="Clean before MAKE.", action='store_const', const=True)
 parser.add_argument("-p", "--pattern", dest="find_pattern", help="Pattern to find.")
+parser.add_argument("-R", "--replace_pattern", dest="replace_pattern", help="Pattern to replace. Only works with PO files and in msgstr entries. Not currently support INVERT match")
+
+parser.add_argument("-CL", "--case_low", dest="case_lower", help="Replaced find pattern with lowercase version in PO tranlation string", action='store_const', const=True)
+parser.add_argument("-CU", "--case_up", dest="case_upper", help="Replaced find pattern with uppercase version in PO tranlation string", action='store_const', const=True)
+parser.add_argument("-CC", "--case_cap", dest="case_capital", help="Replaced find pattern with capital first character version in PO tranlation string", action='store_const', const=True)
+parser.add_argument("-CT", "--case_title", dest="case_title", help="Replaced find pattern with title case (capitalised first character of each word) version in PO tranlation string", action='store_const', const=True)
+
 parser.add_argument("-o", "--po", dest="find_po", help="Find in $BLENDER_MAN_VI/LC_MESSAGES.", action='store_const', const=True)
 parser.add_argument("-r", "--rst", dest="find_rst", help="Find in $BLENDER_MAN_EN/manual.", action='store_const', const=True)
 parser.add_argument("-s", "--src", dest="find_src", help="Find in $BLENDER_ae.", action='store_const', const=True)
@@ -435,12 +557,21 @@ parser.add_argument("-f", "--file", dest="find_file", help="Find in a specific f
 parser.add_argument("-l", "--line", dest="show_line_number", help="Showing matched line number next to the found text.", action='store_const', const=True)
 parser.add_argument("-v", "--invert_match", dest="invert_match", help="Invert match, find text DOESN'T match the input pattern.", action='store_const', const=True)
 parser.add_argument("-O", "--only_match", dest="only_match", help="Listing only the matched part of the text.", action='store_const', const=True)
+parser.add_argument("-t", "--testing_only", dest="testing_only",
+                    help="Print out changes only, but DO NOT write changes. For examinations before commit changes. Only applicable with replacing translations in PO files.",
+                    action='store_const', const=True)
+parser.add_argument("-D", "--debug", dest="debugging", help="Print out messages as processing.", action='store_const', const=True)
 
 args = parser.parse_args()
 
 x = FindFilesHasPattern()
 x.setVars(
     args.find_pattern,
+    args.replace_pattern,
+    args.case_lower,
+    args.case_upper,
+    args.case_capital,
+    args.case_title,
     args.find_file,
     args.find_po,
     args.find_rst,
@@ -451,7 +582,10 @@ x.setVars(
     args.after_lines,
     args.only_match,
     args.show_line_number,
-    args.invert_match
+    args.invert_match,
+    args.testing_only,
+    args.debugging
     )
 
+DEBUG=(True if args.debugging else False)
 x.run()
